67db8fe23febb326f358f5a96d6087d34dc04f11
davidvanr21 <50579850+davidvanr21@users.noreply.github.com>
2022-06-22 18:09:54 +0200

    Content and style added

--- script/main.js
+++ script/main.js
@@ -1,16 +1,17 @@
 console.log("Hello world")
 
+// function myFunction() {
+//   var x = document.getElementById("menu");
+//   if (x.style.display === "none") {
+//     x.style.display = "block";
+//   } else {
+//     x.style.display = "none";
+//   }
+// }
+
 
 // img movement on mousemove
 // reference video: https://www.youtube.com/watch?v=ysF0rFjIk7o
-function myFunction() {
-    var x = document.getElementById("menu");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
-  }
 
 let imgArr = document.querySelectorAll(".img");
 let prevX = 0;
@@ -41,3 +42,12 @@ function moveImg(xAmount, yAmount){
         img.style.top = (img.offsetTop) - (yAmount/movementStrength) + "px";
     })
 }
+
+// World map animation
+var animation = lottie.loadAnimation({
+    container: document.getElementById('worldMap'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'worldmapBlueZones3.json'
+})
